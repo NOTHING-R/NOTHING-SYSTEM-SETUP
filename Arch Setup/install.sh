91b27618011cb70b6934f41fae186da3ede1bd10
NOTHING-R <kalilinux.nothing@gmail.com>
2025-06-02 10:54:00 +0600

    updating the srcipts

---
+++ Arch Setup/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+echo "Setting up yay..."
+cd ~
+sudo pacman -S --needed git base-devel go
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+
+echo "Installing require application..."
+
+sudo pacman -S xorg xorg-xinit xorg-server i3 sddm emacs alacritty nitrogen stow picom qt5-declarative qt5-quickcontrols2 qt5-graphicaleffects
+
+echo "Done!!"
+
+read -p "Do you want to clone the dotfiles repo? (yes/no): " answer
+
+if [[ "$answer" == "yes" || "$answer" == "y" ]]; then
+    echo "cloning dotfiles repo"
+    git clone https://github.com/NOTHING-R/dotfiles.git ~/dotfiles
+else
+    echo "You can found it in https://github.com/NOTHING-R"
+fi
+
--- sddm/install.sh
+++ sddm/install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash 
-echo "Please give the sudo password of your system for root permission"
-sudo su
+if [ "$EUID" -ne 0 ]; then 
+  echo "Please give your root password"
+  exec sudo "$0" "$@"
+fi
+
 sudo pacman -S sddm
 sudo systemctl enable sddm
 mkdir -p /etc/sddm.conf.d/
-touch -p /etc/sddm.conf.d/theme.conf.user
+touch /etc/sddm.conf.d/theme.conf.user
 sudo cp -r nothing_sddm_theme/ /usr/share/sddm/themes/ 
 cp theme.conf.user /etc/sddm.conf.d/
 echo "Installed successfully"
