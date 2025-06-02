6e809a9a70879958c77e6be465ca7de5b3571726
NOTHING-R <kalilinux.nothing@gmail.com>
2025-06-02 21:35:19 +0600

    Updating the install script for automatic exwm installation

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ cd ..
 
 echo "Installing require application..."
 
-sudo pacman -S xorg xorg-xinit xorg-server xorg-xrandr i3 sddm emacs alacritty nitrogen stow picom qt5-declarative qt5-quickcontrols2 qt5-graphicaleffects
+sudo pacman -S xorg xorg-xinit xorg-server xorg-xrandr i3 sddm emacs alacritty nitrogen stow picom make cmake nerd-fonts ttf-jetbrains-mono-nerd qt5-declarative qt5-quickcontrols2 qt5-graphicaleffects
 sudo systemctl enable sddm
 echo "Done!!"
 
@@ -23,3 +23,32 @@ else
     echo "You can found it in https://github.com/NOTHING-R"
 fi
 
+read -p "Do you want to set up EXWM in .xinitrc? (yes/no): " answer
+if [[ "$answer" == "yes" || "$answer" == "y" ]]; then
+    echo "Setting up EXWM in .xinitrc and Xresources..."
+    echo "Xft.dpi: 125" > ~/.Xresources
+
+    cat > ~/.xinitrc <<EOF
+#!/bin/sh
+# Set environment variables or configurations
+
+# Start EXWM via Emacs
+exec emacs
+EOF
+
+    chmod +x ~/.xinitrc
+    echo ".xinitrc configured!"
+
+    echo "Creating EXWM desktop session entry..."
+    sudo bash -c 'cat > /usr/share/xsessions/exwm.desktop <<EOF
+[Desktop Entry]
+Name=EXWM
+Exec=emacs
+Type=Application
+DesktopName=EXWM
+EOF'
+
+    echo "EXWM setup complete!" 
+else
+    echo "Skipping EXWM setup. You can do it manually later."
+fi
